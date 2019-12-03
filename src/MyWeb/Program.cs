d6e7214de74cca2b9c8e33e9d7358afd39bb9dcd
wk-j <somnuk.wk@gmail.com>
2019-12-03 12:01:42 +0700

    Initial commit

---
+++ src/MyWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
